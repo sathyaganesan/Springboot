766f302dbd3c912bebe0620520e48c98beb3752e
Ganesan <sathya.ganesan@accenture.com>
2022-01-20 12:44:13 -0500

    Initial commit from springboot

---
+++ restfulWebServiceDemo/restfulWebServiceDemo/src/test/java/com/example/restfulWebServiceDemo/RestfulWebServiceDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.restfulWebServiceDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulWebServiceDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
